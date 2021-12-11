cc52e8a2d77613c5d18fb5ccf1d6afb9329a59cb
itancastagnola <92515995+itancastagnola@users.noreply.github.com>
2021-12-11 19:06:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include "lib.h"
+
+float massimo (float max[], int n, float m){
+    for (int i=0;i<n; i++){
+        if ( i==0){
+            m=max[i];
+        }else if (i>0 & max[i]>m){
+            m=max[i];
+        }
+    }
+    return 0;
+}
+
+
 
