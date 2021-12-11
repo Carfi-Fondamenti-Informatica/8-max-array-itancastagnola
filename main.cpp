225dd43827d8f99192cff0d26c5740e8bd0fe507
itancastagnola <92515995+itancastagnola@users.noreply.github.com>
2021-12-11 19:05:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+
+using namespace std;
+int main() {
+    int n;
+    float m=0;
+    cin >> n;
+    float max[n];
+    for (int i=0;i<n;i++){
+        float a=0;
+        cin >> a;
+        max[i]=a;
+    }
+    cout << massimo (max,n,m);
+    return 0;
+
 }
+
